f7fcd361bccf36b1aa5b56aa954a2c281936eaff
lordmogul <etimbukudoekong@gmail.com>
2016-04-11 22:38:35 +0100

    Added files via upload

---
+++ index.php
@@ -0,0 +1,462 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Welcome to Spiritan University</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+  </head>
+
+  <body>
+    <div class="text-right">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12">
+            <p class="top_no">
+              +234-568121121
+            </p>
+            <p class="top_link">
+              Contact us | Account Login | Current Students | Faculty & Staff | Parents
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+   	<nav class="navbar navbar-new" role="navigation">
+      <div class="container-fluid">
+        <div class="containr">
+        <!--logo-->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        <!--menu-->
+        <div class="collapse navbar-collapse navbar-right" id="navbar">
+          <ul class="nav navbar-nav">
+              <li class="active"><a href="index.php">Home</a></li>
+              <li><a href="commisions.php">About Us</a></li>
+              <li><a href="why_us.php">Why us</a></li>
+              <li><a href="partners.php">Admissions</a></li>
+              <li><a href="binary_options.php">Faculties & Departments</a></li>
+              <li><a href="blog.php">International</a></li>
+              <li><a href="faqs.php">News & Events</a></li>
+              <li><a href="contact.php">Portal</a></li>
+              <li><a href="contact.php">Contact us</a></li>
+              
+          </ul>
+           <form class="navbar-form navbar-left" role="search"> 
+              <div class="form-group"> 
+                <input type="text" class="form-control" placeholder="Search website..."> 
+              </div> 
+              <button type="submit" class="btn btn-default"><span><img src="img/search.png" class="img-responsive search"></span></button> 
+           </form>   
+        </div>
+        </div>
+      </div>
+    </nav>
+
+    <header>
+      <div class="container-fluid" id="rollers">
+        <div class="container">
+            <div class="row" >
+              <div class="col-sm-8" id="row">
+                <div id="myCarousel" class="carousel slide"> 
+                   <!-- Carousel indicators --> 
+                   <ol class="carousel-indicators"> 
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li> 
+                      <li data-target="#myCarousel" data-slide-to="1"></li> 
+                      <li data-target="#myCarousel" data-slide-to="2"></li> 
+                   </ol>    
+                   <!-- Carousel items --> 
+                   <div class="carousel-inner"> 
+                      <div class="item active"> 
+                         <img src="images/slide1.jpg" alt="First slide"> 
+                         <div class="carousel-caption">This Caption 1</div>
+                      </div> 
+                      <div class="item"> 
+                         <img src="images/slide2.jpg" alt="Second slide">
+                         <div class="carousel-caption">This Caption 2</div> 
+                      </div> 
+                      <div class="item"> 
+                         <img src="images/slide3.jpg" alt="Third slide">
+                         <div class="carousel-caption">This Caption 3</div> 
+                      </div> 
+                   </div> 
+                   <!-- Carousel nav --> 
+                   
+                   <a class="carousel-control left" href="#myCarousel"  
+                      data-slide="prev">&lsaquo;</a> 
+                   <a class="carousel-control right" href="#myCarousel"  
+                      data-slide="next">&rsaquo;</a> 
+                </div>      
+              </div>
+
+              <!-- About Uni -->
+              <div class="col-sm-4">
+                <div class="about_home">
+                  <h3>About us</h3>
+                  <img src="img/vc.jpg" class="img-responsive" />
+                  <p>
+                    <img src="img/quotation_marks_left.png" class="img-responsive">
+                    The theological system of the Holy Scriptures being the subject of our inquiries, it is essential to our undertaking to establish their Divine authority. But before the direct evidence
+                    <img src="img/quotation_marks_right.png" class="img-responsive"> 
+                  </p>
+                  <a class="btn btn-primary" role="button">for more</a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    
+    <section id="section">
+      <div class="container-fluid section">
+        <div class="container">
+          <div class="row" id="basic">
+
+            <!--News and Event-->
+            <div class="col-sm-4" id="months">
+              <h3>News and Events</h3>
+              <img src="img/news_home.jpg" class="img-responsive">
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. <p class="text-right"><b>JAN</b> 20</p>
+                    </p>                  
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+              
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+              <a class="btn btn-primary btn-lg active btn-block" role="button">More News</a>
+
+            </div>
+
+            <!--School calendar-->
+            <div class="col-sm-4" id="months">
+              <h3>Campus News</h3>
+              <img src="img/news_home.jpg" class="img-responsive">
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. <p class="text-right"><b>jan</b> 20</p>
+                    </p>                  
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+              
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+
+              <div class="row" id="news_headings">
+                  <div class="col-sm-2">
+                    <img src="img/news1.jpg" class="img-responsive">
+                  </div>
+                    <p>
+                      Our University begins lectures Sept. Our University begins lectures Sept. Our University begins lectures Sept.<p class="text-right"><b>JAN</b> 20</p>
+                    </p>                 
+              </div>
+              <a class="btn btn-primary btn-lg active btn-block" role="button">More News</a>
+
+            </div>
+
+            <div class="row">
+              <div class="col-sm-4" id="links">
+                <h3>University's Quick Links</h3>
+                <img src="img/news_home.jpg" class="img-responsive">
+                <div class="link1">
+                 <p><a href="#">PROSPECTIVE STUDENTS</a></p>
+                </div>
+                <div class="link2">
+                 <p><a href="#">iTRANSCRIPT</a></p>
+                </div>
+                <div class="link3">
+                 <p><a href="#">NEWSLETTER</a></p>
+                </div>
+                <div class="link4">
+                 <p><a href="#">CHECK ADMISSION LIST</a></p>
+                </div>
+                <div class="link5">
+                 <p><a href="#">2016/2017 ACADEMIC CALENDAR</a></p>
+                </div>
+                <div class="link6">
+                 <p><a href="#">2016/2017 FEES SCHEDULE</a></p>
+                </div>
+                <div class="link7">
+                 <p><a href="#">NEWS - TODAY</a></p>
+                </div>
+                <div class="link8">
+                 <p><a href="#">UPCOMING EVENTS</a></p>
+                </div>
+                <div class="link9">
+                 <p><a href="#">ANNOUNCEMENTS</a></p>
+                </div>
+                <div class="link10">
+                 <p><a href="#">PARTNER INSTITUTIONS</a></p>
+                </div>
+              </div>
+            </div>
+
+           
+          </div>
+
+          
+        </div>
+      </div>
+    </section>
+
+    <section id="section_more">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-12 faculties">
+            <h3>FACULTIES &amp DEPARTMENTS</h3>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="row" id="faculties">
+          <div class="col-sm-4">
+            <h4>FACULTY OF SOCIAL & MANAGEMENT SCIENCES</h4>
+              <img src="img/course_icon.png" class="img-responsive center-block">
+            <ul>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h4>FACULTY OF NATURAL AND APPLIED SCIENCES</h4>
+              <img src="img/course_icon5.jpg" class="img-responsive center-block">
+            <ul>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h4>FACULTY OF EDUCATION</h4>
+              <img src="img/course_icon4.jpg" class="img-responsive center-block">
+            <ul>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h4>FACULTY OF ARTS</h4>
+              <img src="img/course_icon3.jpg" class="img-responsive center-block">
+            <ul>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h4>FACULTY OF LAW</h4>
+              <img src="img/course_icon2.jpg" class="img-responsive center-block">
+            <ul>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+              <li>Department of Economics</li>
+            </ul>
+          </div>
+          <div class="col-sm-4">
+            <h4>FACULTY OF ENGINEERING</h4>
+              <img src="img/course_icon6.jpg" class="img-responsive center-block">
+              <ul>
+                <li>Department of Economics</li>
+                <li>Department of Economics</li>
+                <li>Department of Economics</li>
+                <li>Department of Economics</li>
+                <li>Department of Economics</li>
+              </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section id="section_more">
+      <div class="container-fluid" id="study">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12">
+              <h3>STUDYING AT SPIRITAN UNIVERSITY</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+    <section id="section_more">
+      <div class="container-fluid">
+        <div class="container">
+          <div class="row" id="pad_here">
+            <div class="col-sm-3" id="one">
+              <h4>Maps & Direction</h4>
+              <img src="img/icon-map.png" class="img-responsive">
+              <p>
+                Our University relies on Hardwork, determination and success. We ensure these three attitudes are passed on to our
+                students.
+              </p>
+            </div>
+            <div class="col-sm-3" id="two">
+              <h4>Campus Life</h4>    
+                          <img src="img/campus_life.png" class="img-responsive">
+              <p>
+                Our University relies on Hardwork, determination and success. We ensure these three attitudes are passed on to our
+                students.
+              </p>
+            </div>
+            <div class="col-sm-3" id="three">
+              <h4>Research & Innovation</h4>
+              <img src="img/research.png" class="img-responsive">
+              <p>
+                Our University relies on Hardwork, determination and success. We ensure these three attitudes are passed on to our
+                students.
+              </p>
+            </div>
+            <div class="col-sm-3" id="four">
+              <h4>Prospective Students</h4>
+              <img src="img/prospective_students.png" class="img-responsive">
+              <p>
+                Our University relies on Hardwork, determination and success. We ensure these three attitudes are passed on to our
+                students.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <!--<section id="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6" id="center">
+            <img src="img/commission.png" class="img-responsive">
+            <h2>COMMISSION PLANS</h2>
+            <p>
+              Here at Cherry Revenue, we offer a wide variety of commission packages. All of these are geared towards
+              ensuring that your relationship with us will be smooth, long-lasting and profitable...
+            </p>
+            <a class="btn btn-primary btn-lg active" role="button">for more ></a></p>
+          </div>
+          <div class="col-sm-6" id="center">
+            <img src="img/binary.png" class="img-responsive">
+            <h2>BINARY OPTIONS</h2>
+            <p>
+              Binary options is a style of trading where the trader only speculates on whether the price of an asset is going to rise or fall. The asset could be a currency, stock, indices or commodity. After deciding on whether the asset will rise or fall, you have to decide on the time frame within...
+            </p>
+            <a class="btn btn-primary btn-lg active" role="button">for more ></a></p>
+          </div>
+        </div>
+      </div>
+    </section>-->
+
+    <?php
+      include_once 'includes/footer.php';
+    ?>
+
+    
+   		
+
+
+    <script src="js/jquery.js"></script>	
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- Script to Activate the Carousel -->
+    <script>
+    $('.carousel').carousel({
+        interval: 5000 //changes the speed
+    })
+    </script>
+  </body>
+</html>
